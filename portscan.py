eaa841996c212a878543bf28d576b55ce199dab0
jozzidev <jozzideveloper@gmail.com>
2025-08-19 09:36:09 -0400

    portscan simples
    portscan simples
    
    como usar no terminal 
    nano portscan.py (para editar)
    python3 (executar)

---
+++ portscan.py
@@ -0,0 +1,14 @@
+import socket
+
+ports = [21,22,80,443,445,3306,25]
+
+for port in ports:
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.settimeout(0.1)
+	code = client.connect_ex(("website link ou ip", port))
+	if code == 0:
+		print  (port, "OPEN")
+	else:
+		print  (port, "CLOSED")
+
+
